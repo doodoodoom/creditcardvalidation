d98df8b2c838539a8f8f1b73e218f01fd900f5a3
doodoodoom <madridmatt17@gmail.com>
2019-11-21 21:05:41 -0800

    Create basic input field

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <img src="/amex.svg" style={{ width: "50px" }} />
+      <div>Credit Card Number</div>
+      <input type="text" required minLength="15" maxLength="16" size="30" placeholder="1234 1234 1234 1234"></input>
+      {/* <img src="/amex.svg" style={{ width: "50px" }} />
       <img src="/discover.svg" style={{ width: "50px", marginLeft: "10px" }} />
       <img src="/generic.svg" style={{ width: "50px", marginLeft: "10px" }} />
       <img
@@ -16,7 +18,7 @@ function App() {
       <img src="/visa.svg" style={{ width: "50px", marginLeft: "10px" }} />
       <img src="/check.svg" style={{ width: "35px", marginLeft: "10px" }} />
       <img src="/x.svg" style={{ width: "27px", marginLeft: "10px" }} />
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen!</h2> */}
     </div>
   );
 }
