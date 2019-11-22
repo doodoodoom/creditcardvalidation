f90aa7313d0e83ed83d7593c1a0920b4e858c6b4
doodoodoom <madridmatt17@gmail.com>
2019-11-22 14:00:43 -0800

    Allow state to be updated

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import "./styles.css";
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      number: ''
+    };
+
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+  }
+
+  handleNumberChange(event) {
+    this.setState({ number: event.target.value });
   }
 
   render() {
     return (
       <div className="App">
         <div>Credit Card Number</div>
-        <input type="text" required minLength="15" maxLength="16" size="30" placeholder="1234 1234 1234 1234"></input>
+        <input type="text" required maxLength="19" size="30" placeholder="1234 1234 1234 1234" onChange={this.handleNumberChange}></input>
         {/* <img src="/amex.svg" style={{ width: "50px" }} />
         <img src="/discover.svg" style={{ width: "50px", marginLeft: "10px" }} />
         <img src="/generic.svg" style={{ width: "50px", marginLeft: "10px" }} />
