11a2b9968a2078e016b076c92f99c2bbb5f3cdaf
doodoodoom <madridmatt17@gmail.com>
2019-11-25 14:18:56 -0800

    Install react testing library, test formatting

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render, fireEvent } from '@testing-library/react';
 import App from "./index";
 
 // Test 0 -- Ensure App renders without issue
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+// Test 1 -- Ensure credit card number formats correctly
+it("formats credit card numbers correctly", () => {
+  const { getByPlaceholderText } = render(<App />);
+  fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "1234" } });
+  expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("1234 ");
+  fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "1234 1234" } });
+  expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("1234 1234 ");
+  fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "1234 1234 1234" } });
+  expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("1234 1234 1234 ");
+  fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "1234 1234 1234 1234" } });
+  expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("1234 1234 1234 1234");
 });
