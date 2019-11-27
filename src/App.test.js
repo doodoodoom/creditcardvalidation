726d030680f079753d21249029924cf92d162f72
doodoodoom <madridmatt17@gmail.com>
2019-11-27 03:05:06 -0800

    Initialize error message test

--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,13 @@ import App from "./index";
 jest.setTimeout(20 * 1000);
 
 // Test 0 -- Ensure App renders without issue
-it("renders without crashing", () => {
+xit("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
 
 // Test 1 -- Ensure credit card number formats correctly
-it("formats credit card numbers correctly", () => {
+xit("formats credit card numbers correctly", () => {
   const { getByPlaceholderText } = render(<App />);
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "1234" } });
   expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("1234 ");
@@ -25,7 +25,7 @@ it("formats credit card numbers correctly", () => {
 });
 
 // Test 2 -- Ensure correct card type is received from API and renders
-it("gets and renders the card type correctly", async () => {
+xit("gets and renders the card type correctly", async () => {
   const { getByPlaceholderText, getByAltText } = render(<App />);
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "6011 00" } });
   await waitForElement(() => getByAltText("Type").getAttribute("src") === "/discover.svg", { timeout: 20 * 1000 });
@@ -33,7 +33,7 @@ it("gets and renders the card type correctly", async () => {
 });
 
 // Test 3 -- Ensure check mark appears when card is valid
-it("renders a check mark correctly when the credit card is valid", () => {
+xit("renders a check mark correctly when the credit card is valid", () => {
   const { getByPlaceholderText, getByAltText } = render(<App />);
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "6011 0017 2222 2222" } });
   fireEvent.blur(getByPlaceholderText("1234 1234 1234 1234"));
@@ -41,7 +41,7 @@ it("renders a check mark correctly when the credit card is valid", () => {
 });
 
 // Test 4 -- Ensure x mark appears when card is invalid
-it("renders a check mark correctly when the credit card is valid", () => {
+xit("renders a check mark correctly when the credit card is valid", () => {
   const { getByPlaceholderText, getByAltText } = render(<App />);
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "6011 0017 2222 22" } });
   fireEvent.blur(getByPlaceholderText("1234 1234 1234 1234"));
@@ -56,8 +56,8 @@ it("renders a check mark correctly when the credit card is valid", () => {
   expect(getByAltText("Valid").getAttribute("src")).toEqual("/x.svg");
 });
 
-// Test 5 -- Ensure amex cards format correctly *WORK IN PROGRESS*
-it("formats amex cards correctly", async () => {
+// Test 5 -- Ensure amex cards format correctly
+xit("formats amex cards correctly", async () => {
   const { getByPlaceholderText, getByAltText } = render(<App />);
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "3712" } });
   expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("3712 ");
@@ -72,4 +72,9 @@ it("formats amex cards correctly", async () => {
   expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("3712 345678 ");
   fireEvent.change(getByPlaceholderText("1234 1234 1234 1234"), { target: { value: "3712 345678 95004" } });
   expect(getByPlaceholderText("1234 1234 1234 1234").value).toEqual("3712 345678 95004");
+});
+
+// Test 6 -- Ensure error message appropriately appears
+it("throws an error message when lookup fails and/or provides no data", async () => {
+  const { getByPlaceholderText, getByAltText } = render(<App />);
 });
